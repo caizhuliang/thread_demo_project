9ddd7a0dd31323c60211b4a01b64bc61954187fc
caizhuliang <caizhuliang@qipeipu.com>
2020-03-12 08:55:00 +0800

    根据阿里规范修改代码

--- src/main/java/com/czl/thread/ThreadTest.java
+++ src/main/java/com/czl/thread/ThreadTest.java
@@ -2,24 +2,26 @@ package com.czl.thread;
 
 public class ThreadTest implements Runnable {
 
-	private int tickets = 100;
-	String str = new String("");
+  private int tickets = 100;
+  String str = new String("");
 
-	public void run() {
-		while (true) {
+  @Override
+  public void run() {
+    while (true) {
 //			synchronized(str) {
-				if (tickets > 0) {
-					try {
-						Thread.sleep(10);// 让线程睡眠10毫秒再往下执行（人为放大线程不安全的现象）
-					} catch (InterruptedException e) {
-						e.printStackTrace();
-					}
-					
-					System.out.println(Thread.currentThread().getName() + " is saling ticket " + tickets--);
-				}
-//			}
-		}
+      if (tickets > 0) {
+        try {
+          // 让线程睡眠10毫秒再往下执行（人为放大线程不安全的现象）
+          Thread.sleep(10);
+        } catch (InterruptedException e) {
+          e.printStackTrace();
+        }
 
-	}
+        System.out.println(Thread.currentThread().getName() + " is saling ticket " + tickets--);
+      }
+//			}
+    }
+
+  }
 
 }
